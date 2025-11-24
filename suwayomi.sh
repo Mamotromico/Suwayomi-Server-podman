760d5f305ea5a40dd31429a83122dc6aab54b151
mamotromico <mamotromico@gmail.com>
2025-11-24 11:12:18 -0300

    fix jar path

--- suwayomi.sh
+++ suwayomi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export TZ="${TZ:-Etc/UTC}"
-java -Dsuwayomi.tachidesk.config.server.rootDir="/suwayomi" --enable-native-access=ALL-UNNAMED -jar suwayomi.jar
+java -Dsuwayomi.tachidesk.config.server.rootDir="/suwayomi" --enable-native-access=ALL-UNNAMED -jar /usr/local/bin/suwayomi.jar
