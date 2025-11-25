25c14adecc32908c7d9e79ed1f81eee88a7d585e
mamotromico <mamotromico@gmail.com>
2025-11-25 11:56:45 -0300

    update java call

--- suwayomi.sh
+++ suwayomi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export TZ="${TZ:-Etc/UTC}"
-java -Dsuwayomi.tachidesk.config.server.rootDir="/suwayomi" --enable-native-access=ALL-UNNAMED -jar /usr/local/bin/suwayomi.jar
+java -Dsun.jnu.encoding=UTF-8 -Dfile.encoding=UTF-8 -Dsuwayomi.tachidesk.config.server.rootDir="/suwayomi" --enable-native-access=ALL-UNNAMED -jar /usr/local/bin/suwayomi.jar
